5a084a5d25e5e23d4b18f38c1ea5802f68017c8d
IamSoughtOut0 <soughtouthiswilheight@gmail.com>
2022-04-08 00:33:45 -0700

    task fifth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * *main -Entry point
  *
- * Return: Always 0 (succes)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts(programming);
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
