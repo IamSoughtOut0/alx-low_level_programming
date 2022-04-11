b466aedec3059b4946ed069a76fcaea6bbfba6ad
IamSoughtOut0 <soughtouthiswilheight@gmail.com>
2022-04-11 10:42:32 -0700

    betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /**
+ * main - entry point
  *
+ * Description: Prints a number and it's last digit along with whether its
+ * last digit is 0, greater than 5, or less than 6 and not 0.
+ * Return: 0
  */
 int main(void)
 {
