7709cff68dcbead3cb95ac87604a1e620480e79d
IamSoughtOut0 <soughtouthiswilheight@gmail.com>
2022-04-11 13:41:17 -0700

    betty beta

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print numbers 0 to 9 on the line
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+for (1 = '0'; 1 <= '9'; ++1)
+putchar(i);
+putchar('\n');
+
+return (0);
+}
