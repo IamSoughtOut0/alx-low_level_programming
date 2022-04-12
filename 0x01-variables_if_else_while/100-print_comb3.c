ceba0489edfcb6334776681c5bbd32bb62f8d470
IamSoughtOut0 <soughtouthiswilheight@gmail.com>
2022-04-11 21:42:16 -0700

    tired

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include  <stdio.h>
 /**
  * main - Prints all possible combination of two different digits,
- *        in ascending order, seperated by a comma followed by a space 
+ * in ascending order, seperated by a comma followed by a space 
  * Return: Always 0.
  */
 int main(void)
