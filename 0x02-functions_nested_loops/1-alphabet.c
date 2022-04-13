8c765de7e09b88354eb5cac5f6fabe5dd72530db
IamSoughtOut0 <soughtouthiswilheight@gmail.com>
2022-04-13 04:39:48 -0700

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets in lowercase, followed by the new line.
+ */
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= letter 'z'; letter++)
+_putchar(letter);
+
+putchar('\n');
+}
