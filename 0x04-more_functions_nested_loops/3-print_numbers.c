5a01faffbca6870b78e9542c17630a5ee6563042
IamSoughtOut0 <soughtouthiswilheight@gmail.com>
2022-04-14 08:28:22 -0700

    task4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
-/**
- * print_numbers - print numbers
+/** 
+ * print_number - print numbers
  * Return: Always 0.
  */
 void print_numbers(void)
@@ -9,8 +9,8 @@ int i;
 i = 0;
 while (i < 10)
 {
-_putchar(i + '0');
+_putchar (i + '0');
 i++;
 }
-_putchar('\n')
+_putchar ('\n');
 }
